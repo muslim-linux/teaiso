9925e3d030b399c38ac2d98dc67f8896827cbb3c
sulincix <aliriza.keskin@pardus.org.tr>
2023-09-08 08:08:08 +0000

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -3,8 +3,8 @@ cd /tmp
 
 ### Instally 17g and other stuff
 apt install wget
-wget https://github.com/aleyna-tilki/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
-wget https://github.com/aleyna-tilki/17g-installer/releases/download/current/17g-installer_1.0_all.deb
+wget https://github.com/muslim-linux/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
+wget https://github.com/muslim-linux/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 wget https://github.com/pardus-nosystemd/desktop-base/releases/download/current/desktop-base_9999-noupdate_all.deb
 apt install ./*.deb -yq --allow-downgrades
 
