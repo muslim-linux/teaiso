c94f1cf46a3161781a1bb5c2b7d4d114f216e801
sulincix <aliriza.keskin@pardus.org.tr>
2023-09-08 08:45:37 +0000

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -6,6 +6,7 @@ apt install wget
 wget https://github.com/muslim-linux/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
 wget https://github.com/muslim-linux/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 wget https://github.com/pardus-nosystemd/desktop-base/releases/download/current/desktop-base_9999-noupdate_all.deb
+wget https://github.com/muslim-linux/muslim-backgrounds/releases/download/current/muslim-backgrounds_1.0_all.deb
 apt install ./*.deb -yq --allow-downgrades
 
 #### Disable recommends by default
