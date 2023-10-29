d820725cbd4d8bf944791be8a549e21c21077b0c
sulincix <aliriza.keskin@pardus.org.tr>
2023-10-29 17:25:43 +0000

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -21,3 +21,7 @@ apt install ./*.deb -yq --allow-downgrades
 # fix muezzin icon
 install /usr/share/icons/hicolor/0x0/apps/muezzin.png /usr/share/icons/hicolor/512x512/apps/muezzin.png
 gtk-update-icon-cache /usr/share/icons/hicolor/
+
+# install flatpak
+apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
